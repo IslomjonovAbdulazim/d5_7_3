c3396396bca48dfde1ae2dae3dfd024fb6ca5ef0
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-12 17:36:44 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,18 +11,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffF2F2F2),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
               // Search & Avatar
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Image.asset(
+                    "assets/person.png",
+                    height: 48,
+                    width: 48,
+                  ),
+                ],
+              ),
 
               // Brands
 
               // Popular Cars
-
-
             ],
           ),
         ),
